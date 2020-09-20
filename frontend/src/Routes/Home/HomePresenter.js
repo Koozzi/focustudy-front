f0c348de5be98f53056d5df47d819395f1b11389
SeungHyun Lee <easter3163@naver.com>
2020-09-21 01:41:05 +0900

    .
    .

--- frontend/src/Routes/Home/HomePresenter.js
+++ frontend/src/Routes/Home/HomePresenter.js
@@ -34,7 +34,7 @@ function Home() {
             <div className="container">
                 <div className="row">
                     <div className="column-66">
-                    <h1 className="xlarge-font"><b>The App</b></h1>
+                    <h1 className="xlarge-font"><b>The Apple</b></h1>
                     <h1 className="large-font"><b>Why Focustudy?</b></h1>
                     <p><span>Take photos like a pro.</span> You should buy this app because lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                     <button className="button">Start Application</button>
