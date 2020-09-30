676acf98fb6251cee7b1eaaf46a18c52f2a494b7
Koozzi <kchy666@naver.com>
2020-10-01 02:22:47 +0900

    Add todo page

--- frontend/src/Components/Dashboard_Router.js
+++ frontend/src/Components/Dashboard_Router.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 
 import Dashboard_Header from '../Routes/Dashboard_Header/Dashboard_Header';
-import Dashboard_Social from '../Routes/Dashboard_Social/Dashboard_Social';
-import Dashboard_Profile from '../Routes/Dashboard_Profile/Dashboard_Profile';
 import Dashboard_Home from '../Routes/Dashboard_Home/Dashboard_Home';
 import Study from '../Routes/Study';
+import Dashboard_Social from '../Routes/Dashboard_Social/Dashboard_Social';
+import Dashboard_Profile from '../Routes/Dashboard_Profile/Dashboard_Profile';
+import Dashboard_Todo from '../Routes/Dashboard_Todo/Dashboard_Todo';
 
 export default function Dashboard_Router() {
     return (
@@ -16,6 +17,7 @@ export default function Dashboard_Router() {
                 <Route path="/social" exact component={Dashboard_Social} />
                 <Route path="/study" exact component={Study} />
                 <Route path="/profile" exact component={Dashboard_Profile} />
+                <Route path="/todo" exact component={Dashboard_Todo} />
             </Switch>
         </Router>
     )
---
+++ frontend/src/Routes/Dashboard_Todo/Dashboard_Todo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dashboard_Todo() {
+    return (
+        <div>
+            Todo
+        </div>
+    )
+}
