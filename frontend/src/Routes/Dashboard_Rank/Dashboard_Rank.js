7be0a84405b1e6f5eba206c8d6f8010839696166
Koozzi <kchy666@naver.com>
2020-10-06 14:57:14 +0900

    Rank

--- frontend/src/Routes/Dashboard_Rank/Dashboard_Rank.js
+++ frontend/src/Routes/Dashboard_Rank/Dashboard_Rank.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import Axios from 'axios';
 
 export default function Dashboard_Rank() {
+    const [top10TotalScore, setTop10TotalScore] = useState([]);
+
+    const GetTop10 = async() => {
+        const total_10 = await Axios.post(
+            "http://localhost:5050/rank/total_rank",
+        );
+
+        await total_10.data.forEach((Element)=>{
+            console.log(Element);
+            setTop10TotalScore([
+                ...top10TotalScore,
+                {
+                    id: top10TotalScore.length,
+                    displayName: Element.displayName,
+                    totalScore: Element.totalScore
+                }
+            ]);
+        });
+
+        console.log(top10TotalScore);
+    }
+
+    useEffect(()=>{
+        GetTop10();
+    }, []);
+
     return (
         <div>
-            Rank
+            
         </div>
     )
 }
