72d94c21240535b371b340b3bca8f373302fa462
Koozzi <kchy666@naver.com>
2020-10-10 02:10:04 +0900

    Design

--- frontend/src/Routes/Study/StudyPresenter.js
+++ frontend/src/Routes/Study/StudyPresenter.js
@@ -9,7 +9,7 @@ import logo2 from "./assets/multi.jpg";
 
 export default function SelectStudy() {
     const history = useHistory();
-
+ 
     const Multi = () => history.push("/focus/study/listroom");
     const Single = () => history.push("/focus/study/listroom");
 
@@ -19,13 +19,19 @@ export default function SelectStudy() {
             <h1 className="title"> Study</h1>
             <p>"성공하기 위해 지녀야 할 자질이 있는데 이는 명확한 목표,목표에 대한 지식, 성취하고 자 하는 불타는 열망이다." -나폴레옹 힐-</p>
             <div className="study_button">
-                <span className="Button">
-                    <BsFillPersonFill/>
-                    <div onClick={Multi}>Single</div>
+                <span className="List">
+                    <div className="Text">
+                        <BsFillPersonFill/>
+                        <div>Single</div>
+                    </div>
+                    <button className="Button" onClick={Multi}>Start</button>
                 </span>
-                <span className="Button">
-                    <BsPeopleFill/>
-                    <div onClick={Multi}>Multi</div>
+                <span className="List">
+                    <div className="Text">
+                        <BsPeopleFill/>
+                        <div>Multi</div>
+                    </div>
+                    <button className="Button" onClick={Multi}>Start</button>
                 </span>
             </div>
         </div>
