5ea1c3a8c3611c20fe1cb37e2918749432d7a865
Koozzi <kchy666@naver.com>
2020-10-10 01:31:59 +0900

    Design

--- frontend/src/Routes/Study/StudyPresenter.js
+++ frontend/src/Routes/Study/StudyPresenter.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { useHistory } from 'react-router-dom';
 import { MdFilterCenterFocus } from "react-icons/md";
+import { BsPeopleFill, BsFillPersonFill } from "react-icons/bs"
 import "./Study.css";
 import { Container} from '@material-ui/core';
 import logo from "./assets/single.jpg";
@@ -18,8 +19,14 @@ export default function SelectStudy() {
             <h1 className="title"> Study</h1>
             <p>"성공하기 위해 지녀야 할 자질이 있는데 이는 명확한 목표,목표에 대한 지식, 성취하고 자 하는 불타는 열망이다." -나폴레옹 힐-</p>
             <div className="study_button">
-                <button className="single" onClick={Multi}>Single</button>
-                <button className="multi" onClick={Multi}>Multi</button>
+                <span className="Button">
+                    <BsFillPersonFill/>
+                    <div onClick={Multi}>Single</div>
+                </span>
+                <span className="Button">
+                    <BsPeopleFill/>
+                    <div onClick={Multi}>Multi</div>
+                </span>
             </div>
         </div>
         // <Container maxwidth="sm">
