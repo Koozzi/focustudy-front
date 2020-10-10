4f2229c449187ce724f5c45340e297c62f03fd1c
Koozzi <kchy666@naver.com>
2020-10-10 15:27:02 +0900

    design header

--- frontend/src/Routes/Dashboard_Header/Dashboard_Header.js
+++ frontend/src/Routes/Dashboard_Header/Dashboard_Header.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom';
-import styled from "styled-components";
+import { useHistory, NavLink } from 'react-router-dom';
 
 import "./Dashboard_Header.css";
 import { TiHome } from "react-icons/ti";
@@ -8,30 +7,20 @@ import { CgProfile } from "react-icons/cg"
 import { BsPeopleFill, BsListCheck, BsPencilSquare } from "react-icons/bs"
 import { BiMedal } from "react-icons/bi"
 
-export default function Dashboard_Header( {match} ) {
-    const [value, setValue] = useState('Home');
-    const history = useHistory();
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-      };
-
-    const home = () => history.push("/focus");
-    const study = () => history.push("/focus/study");
-    const profile = () => history.push("/focus/profile");
-    const social = () => history.push("/focus/social");
-    const todo = () => history.push("/focus/todo");
-    const rank = () => history.push("/focus/rank");
-
+export default function Dashboard_Header () {
     return (
-        <div className="dashboard-navbar" value={value} onChange={handleChange}>
-            <h1 className="RealHome"><a href="/">FocuStudy</a></h1>
-            <li onClick={home}><TiHome/> Home</li>
-            <li onClick={study}><BsPencilSquare/> Study</li>
-            <li onClick={profile}><CgProfile/> Profile</li>
-            <li onClick={social}><BsPeopleFill/> Social</li>
-            <li onClick={todo}><BsListCheck/> To-Do</li>
-            <li onClick={rank}><BiMedal/> Rank</li>
-        </div>
+        <>
+        <ui className="dashboard-navbar">
+            <h1 className="Dash_RealHome"><a href="/">FocuStudy</a></h1>
+            <div className="aaa">
+                <NavLink exact activeClassName="active_class" className="Menu" to="/focus"><TiHome/> Home</NavLink>
+                <NavLink exact activeClassName="active_class" className="Menu" to="/focus/study"><BsPencilSquare/> Study</NavLink>
+                <NavLink exact activeClassName="active_class" className="Menu" to="/focus/profile"><CgProfile/> Profile</NavLink>
+                <NavLink exact activeClassName="active_class" className="Menu" to="/focus/social"><BsPeopleFill/> Social</NavLink>
+                <NavLink exact activeClassName="active_class" className="Menu" to="/focus/todo"><BsListCheck/> To-Do</NavLink>
+                <NavLink exact activeClassName="active_class" className="Menu" to="/focus/rank"><BiMedal/> Rank</NavLink>
+            </div>
+        </ui>
+        </>
     )
 }
