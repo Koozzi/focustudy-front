79598fb4f8bdfceab696db93bfd4b98140f98712
Koozzi <kchy666@naver.com>
2020-10-10 17:37:57 +0900

    Design

--- frontend/src/Routes/Dashboard_Home/Dashboard_Home.js
+++ frontend/src/Routes/Dashboard_Home/Dashboard_Home.js
@@ -11,7 +11,8 @@ export default function Dashboard_Home() {
         <div>
             <div className="Dashboard-Home-Text">
                 <p>FocuStudy는 여러분을 항상 응원합니다.</p>
-                <p>세상에 없던 AI 집중력 타이머, 지금 바로 경험해보세요.</p>
+                <p className="hi">세상에 없던 AI 집중력 타이머,</p>
+                <p>지금 바로 경험해보세요.</p>
             </div>
             <div className="Dashboard-Home-Button">
                 <span onClick={study}>당장 공부하러 가기</span>
