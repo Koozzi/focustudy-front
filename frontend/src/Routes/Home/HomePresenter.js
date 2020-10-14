cca1b22985aecd97914dfdba236bac55c94df7ca
SeungHyun Lee <easter3163@naver.com>
2020-10-14 11:35:38 +0900

    .
    .

--- frontend/src/Routes/Home/HomePresenter.js
+++ frontend/src/Routes/Home/HomePresenter.js
@@ -32,110 +32,33 @@ function Home() {
     return (
         <React.Fragment>
         <Header />
-        <Container maxwidth="sm">
-            <div className="container">
-                <div className="row">
-                    <div className="column-66">
-                    <h1 className="xlarge-font"><b>The Apple</b></h1>
-                    <h1 className="large-font"><b>Why Focustudy?</b></h1>
-                    <p><span>Take photos like a pro.</span> You should buy this app because lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    <button className="button">Start Application</button>
-                    </div>
-                    <div className="column-33">
-                        <img src={logo} width="335" height="471"/>
-                    </div>
-                </div>
+        <div>
+        <div id="light" class="white_content"> <p class = "pop-up">Clone page has pop-up. So do i.</p>
+            <a href = "javascript:void(0)" onclick = "document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">Close</a></div>
+            <div id="fade" class="black_overlay"></div>
+        <div class="frame">
+            <video id="backvid" muted autoplay loop>
+                <source src="https://www.youtube.com/watch?v=2Gg6Seob5Mg&list=PLGmxyVGSCDKvmLInHxJ9VdiwEb82Lxd2E&index=10" type="video/mp4"/>
+                <strong>Your browser does not support the video tag</strong>
+            </video>
+            <div class="header">
+                <div class="up-logo"><a href="../index.html" class="logo-link">DSC UOS</a></div>
             </div>
-            <div className="container">
-                <div className="row">
-                    <div className="column-33">
-                        <img src={logo2} alt="App" width="335" height="471"/>
-                    </div>
-                    <div className="column-66">
-                        <h1 className="xlarge-font"><b>Clarity</b></h1>
-                        <h1 className="large-font"><b>Pixels, who?</b></h1>
-                        <p><span>A revolution in resolution.</span> Sharp and clear photos with the world's best photo engine, incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquipex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                        <button className="button">Read More</button>
-                    </div>
-                </div>
-            </div>
-            <h1 className="xlarge-font"><b>Process</b></h1>
-            <Timeline align="alternate">
-                <TimelineItem>
-                    <TimelineOppositeContent>
-                    <Typography variant="body2" color="textSecondary">
-                        00:00
-                    </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                    <TimelineDot>
-                        <FastfoodIcon />
-                    </TimelineDot>
-                    <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>
-                    <Paper elevation={3} className={classes.paper}>
-                        <Typography variant="h6" component="h1">
-                        Start Your Routine
-                        </Typography>
-                        <Typography>Because you need strength</Typography>
-                    </Paper>
-                    </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                    <TimelineSeparator>
-                    <TimelineDot color="primary">
-                        <LaptopMacIcon />
-                    </TimelineDot>
-                    <TimelineConnector />
-                    </TimelineSeparator>
-                    <TimelineContent>
-                    <Paper elevation={3} className={classes.paper}>
-                        <Typography variant="h6" component="h1">
-                        Do your best with our Timer
-                        </Typography>
-                        <Typography>Because it&apos;s awesome!</Typography>
-                    </Paper>
-                    </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                    <TimelineOppositeContent>
-                    <Typography variant="body2" color="textSecondary">
-                        00:25
-                    </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                    <TimelineDot color="primary" variant="outlined">
-                        <HotelIcon />
-                    </TimelineDot>
-                    <TimelineConnector className={classes.secondaryTail} />
-                    </TimelineSeparator>
-                    <TimelineContent>
-                    <Paper elevation={3} className={classes.paper}>
-                        <Typography variant="h6" component="h1">
-                        Take a rest
-                        </Typography>
-                        <Typography>Because you need rest</Typography>
-                    </Paper>
-                    </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                    <TimelineSeparator>
-                    <TimelineDot color="secondary">
-                        <RepeatIcon />
-                    </TimelineDot>
-                    </TimelineSeparator>
-                    <TimelineContent>
-                    <Paper elevation={3} className={classes.paper}>
-                        <Typography variant="h6" component="h1">
-                        Repeat
-                        </Typography>
-                        <Typography>Because this is the life you love!</Typography>
-                    </Paper>
-                    </TimelineContent>
-                </TimelineItem>
-                </Timeline>
-            </Container>
+            <ul class="btns">
+                <li class="left-btn">
+                    <em>Frontend</em>
+                    <div class="btn-letter">프론트엔드</div>
+                    <div class="rectangle">바로가기</div>
+                </li>
+                <li class="right-btn">
+                    <em>Backend</em>
+                    <div class="btn-letter">백엔드</div>
+                    <div class="rectangle">바로가기</div>
+                </li>
+            </ul>
+            <p class="slogan">Connectiong the Dots</p>
+        </div>
+        </div>
         </React.Fragment>
         
         
