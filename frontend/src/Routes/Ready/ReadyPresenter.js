c2e0197cf4f94df39200030b9a3be73a082a9019
SeungHyun Lee <easter3163@naver.com>
2020-10-30 14:21:35 +0900

    Video Room Design
    Video Room Design

--- frontend/src/Routes/Ready/ReadyPresenter.js
+++ frontend/src/Routes/Ready/ReadyPresenter.js
@@ -4,9 +4,9 @@ import "./Ready.css";
 const ReadyPresenter = ({roomNumber})=> (
   <Container maxwidth="sm">
     <div className="roomtitle"><h1> {roomNumber} </h1></div>
-    <button className="startbutton" id="enterBtn" href="#">ENTER</button>
     <div className="othervideos" id="otherVideos"></div>
     <div className="timer" id="timer">Pomodoro</div>
+    <button className="startbutton" id="enterBtn" href="#">ENTER</button>
 
     <div className="row">
       <div className="column">
