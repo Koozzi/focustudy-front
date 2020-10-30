32e20a4fcb6462e468f2fddf77f3d504f9070146
SeungHyun Lee <easter3163@naver.com>
2020-10-30 14:14:45 +0900

    Video Room Design
    Design

--- frontend/src/Routes/Ready/ReadyPresenter.js
+++ frontend/src/Routes/Ready/ReadyPresenter.js
@@ -3,19 +3,19 @@ import { Container} from '@material-ui/core';
 import "./Ready.css";
 const ReadyPresenter = ({roomNumber})=> (
   <Container maxwidth="sm">
-    <h1>공부를 그만하다니, 인성문제 있어? <button className="startbutton" id="enterBtn" href="#">ENTER</button></h1>
-    <div className="roomtitle"><p>You entered in Room {roomNumber} </p></div>
+    <div className="roomtitle"><h1> {roomNumber} </h1></div>
+    <button className="startbutton" id="enterBtn" href="#">ENTER</button>
+    <div className="othervideos" id="otherVideos"></div>
+    <div className="timer" id="timer">Pomodoro</div>
+
     <div className="row">
       <div className="column">
-        <div className="timer" id="timer">Pomodoro</div>
-      </div>
-      <div className="column">
-        <div> Your Pomodoro score is <div id="facenum"></div></div>
         <video id="myVideo" className="remote-video" autoPlay muted controls playsInline></video>
       </div>
+      <div className="column">
+        <div> My message is <div id="facenum"></div></div>
+      </div>
     </div>
-    <div className="othervideos" id="otherVideos"></div>
-    <canvas id="myChart"></canvas>
   </Container>
 );
 export default ReadyPresenter;
