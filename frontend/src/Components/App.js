dceeee1216f32fd2c6776afbcad5032489836ba8
SeungHyun Lee <easter3163@naver.com>
2020-11-07 04:34:36 +0900

    .
    .

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -3,7 +3,10 @@ import Router from './Router';
 
 function App() {
   return (
+    
+
     <div className="App">
+    <head><script data-ad-client="ca-pub-9742360312927367" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script></head>
       <Router />
     </div>
   );
--- frontend/src/Routes/Home/HomePresenter.js
+++ frontend/src/Routes/Home/HomePresenter.js
@@ -18,7 +18,6 @@ function Home() {
     const classes = useStyles();
     return (
         <React.Fragment>
-          <head><script data-ad-client="ca-pub-9742360312927367" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script></head>
         
         <div class="frame">
             <img id="backvid" src={homeimage}></img>
